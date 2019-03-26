f7ecfa8eaf52f6b6afc113782ba993a6cf6fa192
u211-16 <u211-16@public.mpei.local>
2019-03-26 16:10:52 +0300

    code: минимум

--- main.cpp
+++ main.cpp
@@ -5,16 +5,19 @@ using namespace std;
 int main()
 {
    cout << "Enter A and B: ";
-int a, b,max;
+int a, b,max,min;
 cin>>a>>b;
 max=a;
+min=b;
 if(a<b)
  {
         max=b;
+        min=a;
 }
 cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
      << "A / B = " << a / b << '\n'
-     << "Max = " <<max<< '\n';
+     << "Max = " << max << '\n'
+    << "Min = " << min << '\n';
 }
