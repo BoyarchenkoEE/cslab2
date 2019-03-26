191dd6bb4ee41cc0f4768458194e95633c5f070d
Alice(BoyarchenkoEE) <baee@hgs.com>
2019-03-26 16:05:11 +0300

    code: максимум

--- main.cpp
+++ main.cpp
@@ -5,9 +5,16 @@ using namespace std;
 int main()
 {
    cout << "Enter A and B: ";
-int a, b;
+int a, b,max;
+cin>>a>>b;
+max=a;
+if(a<b)
+ {
+        max=b;
+}
 cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
-     << "A / B = " << a / b << '\n';
+     << "A / B = " << a / b << '\n'
+     << "Max = " <<max<< '\n';
 }
