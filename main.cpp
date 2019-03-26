e0d8cfaee42f08a399dd627559ea0d42dc40b643
u211-16 <u211-16@public.mpei.local>
2019-03-26 15:57:33 +0300

    code: умножение

--- main.cpp
+++ main.cpp
@@ -8,6 +8,5 @@ int main()
 int a, b;
 cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
-     << "A * B = " << a * b << '\n'
-     << "A / B = " << a / b << '\n';
+     << "A * B = " << a * b << '\n';
 }
